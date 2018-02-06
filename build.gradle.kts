ed1d5f4ff0db85657e14102d3eef9354a4dbfa0f
Forrest Hopkins <fingerheroes@gmail.com>
2018-02-06 16:51:34 -0700

    Formatting rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,47 +2,47 @@ import org.gradle.script.lang.kotlin.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
-    var kotlin_version: String by extra
-    kotlin_version = "1.2.21"
-
-    repositories {
-        mavenCentral()
-    }
-    dependencies {
-        classpath(kotlin("gradle-plugin", kotlin_version))
-    }
+	var kotlin_version: String by extra
+	kotlin_version = "1.2.21"
+	
+	repositories {
+		mavenCentral()
+	}
+	dependencies {
+		classpath(kotlin("gradle-plugin", kotlin_version))
+	}
 }
 
 group = "com.forresthopkinsa"
 version = "0.1-SNAPSHOT"
 
 plugins {
-    java
-    application
+	java
+	application
 }
 
 apply {
-    plugin("kotlin")
+	plugin("kotlin")
 }
 
 val kotlin_version: String by extra
 
 application {
-    mainClassName = "MainKt"
+	mainClassName = "MainKt"
 }
 
 repositories {
-    mavenCentral()
-    jcenter()
+	mavenCentral()
+	jcenter()
 }
 
 dependencies {
-    compile(kotlin("stdlib-jdk8", kotlin_version))
-    compile("com.squareup.okhttp3", "okhttp", "3.9.1")
-    compile("com.google.code.gson", "gson", "2.8.2")
-    compile("io.github.config4k", "config4k", "0.3.2")
+	compile(kotlin("stdlib-jdk8", kotlin_version))
+	compile("com.squareup.okhttp3", "okhttp", "3.9.1")
+	compile("com.google.code.gson", "gson", "2.8.2")
+	compile("io.github.config4k", "config4k", "0.3.2")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+	kotlinOptions.jvmTarget = "1.8"
 }
